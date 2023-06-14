4bce2a8ac3a9d73d7df8aad802f130b2da8a11b8
Mohammed-Azad <mhammadazad2016@gmail.com>
2023-06-14 13:52:59 +0300

    modify some design and add restart button

--- lib/answer.dart
+++ lib/answer.dart
@@ -8,12 +8,13 @@ class Answer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
       width: double.infinity,
       margin: EdgeInsets.all(2),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           shape: BeveledRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(5))),
+              borderRadius: BorderRadius.all(Radius.circular(3))),
         ),
         child: Text(answers),
         onPressed: handler(),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/quiz.dart';
+import 'package:restart_app/restart_app.dart';
 import './answer.dart';
 
 void main() {
@@ -7,14 +8,26 @@ void main() {
 }
 
 class QuizApp extends StatefulWidget {
-
   @override
   State<QuizApp> createState() => _QuizAppState();
 }
 
 class _QuizAppState extends State<QuizApp> {
-
   var _questionIndex = 0;
+
+  void restartApp() {
+    
+  }
+
+  void _reset() {
+  Navigator.pushReplacement(
+    context,
+    PageRouteBuilder(
+      transitionDuration: Duration.zero,
+      pageBuilder: (_, __, ___) => QuizApp(),
+    ),
+  );
+}
 
   void _answerQuestion() {
     setState(() {
@@ -27,6 +40,7 @@ class _QuizAppState extends State<QuizApp> {
       print('We are done!');
     }
   }
+
   final _questions = [
     {
       'question': "How Old Are you ?",
@@ -42,25 +56,36 @@ class _QuizAppState extends State<QuizApp> {
     },
   ];
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Quiz App"),
-        ),
-        body: Center(
-        
-        child:_questionIndex < _questions.length
-            ? Quiz( 
-                answerQuestion: _answerQuestion,
-                questionIndex: _questionIndex,
-                questions: _questions,
-              )
-            : Center(child: Text("The End",style: TextStyle(fontSize: 22),),),
-      ),
-      ),
+          appBar: AppBar(
+            title: Text("Quiz App"),
+          ),
+          body: Center(
+            child: _questionIndex < _questions.length
+                ? Quiz(
+                    answerQuestion: _answerQuestion,
+                    questionIndex: _questionIndex,
+                    questions: _questions,
+                  )
+                : resett(),
+          )),
     );
   }
 }
+
+class resett extends StatelessWidget {
+  const resett({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: FloatingActionButton(
+            onPressed: () => Restart.restartApp(), child: Icon(Icons.repeat_sharp)));
+  }
+}
+
+
